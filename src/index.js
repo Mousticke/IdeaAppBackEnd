f0b7454ecf3fef740edef8e483e704aef17a7bc8
Akim Benchiha <akim.benchiha@gmail.com>
2020-02-23 14:53:14 +0100

    Add json middleware

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ router.use(function (req, res, next) {
     next()
 });
 
+app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', api); 
 app.use(redirectHome);
